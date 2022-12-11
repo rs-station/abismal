ab76af1a7483ce7abc298a40cbfe92931b626491
Kevin Dalton <kmdalton@pop-os.localdomain>
2022-12-11 10:47:02 -0500

    add stills test;rename data files

--- tests/io/test_io.py
+++ tests/io/test_io.py
@@ -5,5 +5,21 @@ from abismal.io import MTZLoader, StillsLoader
 
 
 def test_mtz_loader():
-    loader = MTZLoader("test_data.mtz")
+    loader = MTZLoader("conventional.mtz")
     ds = loader.get_dataset()
+
+def test_stills_loader():
+    loader = StillsLoader(
+        [
+            "stills.expt",
+            "stills.expt",
+            "stills.expt",
+        ],
+        [
+            "stills.refl",
+            "stills.refl",
+            "stills.refl",
+        ],
+    )
+    ds = loader.get_dataset()
+
