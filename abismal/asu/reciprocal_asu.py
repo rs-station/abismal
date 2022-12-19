d4408a4849537180d7ff127fca33e0a32ceb8840
Kevin Dalton <kmdalton@pop-os.localdomain>
2022-12-19 13:56:12 -0500

    Add ReciprocalASUCollection and tests

--- abismal/asu/reciprocal_asu.py
+++ abismal/asu/reciprocal_asu.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow_probability import distributions as tfd
 import numpy as np
-from tensorflow import keras
+from tensorflow import keras as tfk
 from reciprocalspaceship.utils import hkl_to_asu,is_absent
 from functools import wraps
 from inspect import signature
@@ -11,8 +11,22 @@ class ReciprocalASUCollection(object):
     def __init__(self, *reciprocal_asus):
         self.reciprocal_asus = reciprocal_asus
 
-    def gather(self, asu_id, hkl):
-        pass
+    def gather(self, tensors, asu_id, hkl):
+        out = None
+        for i,(tensor, rasu) in enumerate(zip(tensors, self.reciprocal_asus)):
+            miller_ids = rasu._miller_ids(hkl)
+            aidx = (miller_ids >= 0) & (asu_id == i)
+            miller_ids = tf.maximum(miller_ids, 0)
+            vals = tf.gather(tensor, miller_ids)
+            if out is None:
+                out = vals
+            else:
+                out = tf.where(
+                    aidx, 
+                    vals,
+                    out,
+                )
+        return out
 
 class ReciprocalASU(object):
     @cellify
@@ -85,21 +99,29 @@ class ReciprocalASU(object):
         self.epsilon = go.epsilon_factor_array(self.Hunique).astype(np.float32)
         self.centric = go.centric_flag_array(self.Hunique).astype(bool)
 
-    @classmethod
-    def from_reference_data(cls, mtz_filename, dmin=None, anomalous=True):
-        import reciprocalspaceship as rs
-        ds = rs.read_mtz(mtz_filename)
-        if anomalous:
-            ds = ds.stack_anomalous()
-        ds = ds.dropna().compute_dHKL()
-        if dmin is None:
-            dmin = ds.dHKL.min()
-        ds = ds[ds.dHKL >= dmin]
-        Hunique = ds.get_hkls().astype('int64')
-        return cls(ds.cell, ds.spacegroup, dmin, anomalous, Hunique)
+    def _ensure_in_range(self, H):
+        """ Cast any out of bounds indices to [0,0,0] """
+        idx = tf.reduce_any(tf.abs(H) > self.hmax, axis=-1, keepdims=True)
+        out = tf.where(
+            idx,
+            tf.zeros_like(H),
+            H
+        )
+        return out
+
+    def _wrap_negative_millers(self, H):
+        """ Wrap negative miller indices to positive for gather """
+        return tf.where(H >= 0, H, H + self.grid_size[...,:])
+
+    def _sanitize_millers(self, H):
+        H = self._ensure_in_range(H)
+        H = self._wrap_negative_millers(H)
+        return H
 
     def _miller_ids(self, H):
-        return tf.gather_nd(self.miller_id, tf.where(H >= 0, H, H + self.grid_size[...,:]))
+        H = tf.cast(H, 'int32')
+        hplus = self._sanitize_millers(H)
+        return tf.gather_nd(self.miller_id, hplus)
 
     def gather(self, tensor, H):
         """
@@ -112,43 +134,3 @@ class ReciprocalASU(object):
         """
         return tf.gather(tensor, self._miller_ids(H))
 
-if __name__=="__main__":
-    import gemmi
-    from IPython import embed
-    sg = gemmi.SpaceGroup(19)
-    cell = gemmi.UnitCell(10., 20., 30., 90., 90., 90.)
-    anomalous = True
-    dmin = 2.
-    L = ReciprocalASU(cell, sg, dmin, anomalous)
-    h,k,l = L.hmax
-    H = np.mgrid[-h:h+1:1,-k:k+1:1,-l:l+1:1].reshape((3, -1)).T
-
-    #Apply resolution cutoff and remove absences
-    d = cell.calculate_d_array(H)
-    H = H[d >= dmin]
-    H = H[~is_absent(H, sg)]
-
-    Hasu,Isym = hkl_to_asu(H, sg)
-    if anomalous:
-        Hasu = np.array([-1, 1])[Isym % 2][:,None]*Hasu
-
-    Hunique,inv = np.unique(Hasu, axis=0, return_inverse=True)
-    miller_ids = np.arange(inv.max() + 1)[inv].astype(np.float32)
-    
-    #Check broadcasting for single 
-    assert L._miller_ids(H[0]) == miller_ids[0]
-
-    #Check broadcasting for multiple
-    assert np.array_equal(L._miller_ids(H).numpy() , miller_ids)
-
-    #Check broadcasting for batched data
-    from tqdm import trange
-    for i in trange(100):
-        size = 10000
-        H = H[np.random.choice(len(H), size)].astype(np.int32)
-        #batch = np.sort(np.random.choice(np.random.choice([-1,0,1])+10, size)).astype(np.int32)
-        batch = np.sort(np.random.choice(10, size)).astype(np.int32)
-        R = tf.RaggedTensor.from_value_rowids(H, batch)
-        ids = L._miller_ids(R)
-
-    embed(colors='Linux')
