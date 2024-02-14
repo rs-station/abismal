e991117f3320a283a28cb4b556f35447c2f18349
Kevin Dalton <kmdalton@pop-os.localdomain>
2024-02-14 11:36:34 -0500

    add init for PE layer

--- abismal/layers/__init__.py
+++ abismal/layers/__init__.py
@@ -1,3 +1,4 @@
 from .skip import FeedForward
 from .combinations import ConvexCombination,ConvexCombinations,Average
 from .transformer import TransformerAverage
+from .positional_encoding import ScaleRange,PositionalEncoding
