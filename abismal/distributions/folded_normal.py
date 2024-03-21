fixes issue with kl weight

abismal/distributions/folded_normal.py
@@ -13,15 +13,12 @@ from tensorflow_probability.python.internal import samplers
 from tensorflow_probability import math as tfm
 from tensorflow_probability.python.internal import tensor_util
 
-def reparameterization_gradient(z, loc, scale):
-    #loc = tf.abs(loc)
-
+def sample_gradients(z, loc, scale):
     alpha = (z + loc) / scale
     beta = (z - loc) / scale
 
-    p = tfd.Normal(0., 1.)
-    p_a = tfd.Normal(loc, scale).prob(z)   #log N(z|loc,scale)
-    p_b = tfd.Normal(-loc, scale).prob(z)  #log N(z|-loc,scale)
+    p_a = tfd.Normal(loc, scale).prob(z)   #N(z|loc,scale)
+    p_b = tfd.Normal(-loc, scale).prob(z)  #N(z|-loc,scale)
 
     # This formula is dz = N(z|-loc,scale) + N(z|loc,scale)
     dz = p_a + p_b
@@ -33,20 +30,13 @@ def reparameterization_gradient(z, loc, scale):
     dscale = -alpha * p_b - beta * p_a
     return dz, dloc, dscale
 
-def cdf(x, loc, scale):
-    x = tf.convert_to_tensor(x)
-    a = (x + loc) / scale
-    b = (x - loc) / scale
-    ir2 = tf.constant(tf.math.reciprocal(tf.sqrt(2.)), dtype=x.dtype)
-    return 0.5 * (tf.math.erf(ir2 * a) - tf.math.erf(ir2 * b))
-
 
 @tf.custom_gradient
 def stateless_folded_normal(shape, loc, scale, seed):
     z = tf.random.stateless_normal(shape, seed, mean=loc, stddev=scale)
     z = tf.abs(z)
     def grad(upstream):
-        grads = reparameterization_gradient(z, loc, scale)
+        grads = sample_gradients(z, loc, scale)
         dz,dloc,dscale = grads[0], grads[1], grads[2]
         dloc = tf.reduce_sum(-upstream * dloc / dz, axis=0)
         dscale = tf.reduce_sum(-upstream * dscale / dz, axis=0)
@@ -123,7 +113,11 @@ class FoldedNormal(tfd.Distribution):
 
     def _cdf(self, x):
         loc,scale = self.loc,self.scale
-        return cdf(x, loc, scale)
+        x = tf.convert_to_tensor(x)
+        a = (x + loc) / scale
+        b = (x - loc) / scale
+        ir2 = tf.constant(tf.math.reciprocal(tf.sqrt(2.)), dtype=x.dtype)
+        return 0.5 * (tf.math.erf(ir2 * a) - tf.math.erf(ir2 * b))
 
     def _sample_n(self, n, seed=None):
         seed = samplers.sanitize_seed(seed)

abismal/layers/normalization.py
@@ -30,8 +30,9 @@ class RunningMoments:
         return np.sqrt(self.var)
 
 class Standardize(tfk.layers.Layer):
-    def __init__(self, center=True, max_counts=np.inf):
+    def __init__(self, center=True, max_counts=np.inf, epsilon=1e-6):
         super().__init__()
+        self.epsilon = epsilon
         self.center = center
         self.max_counts = max_counts
 
@@ -74,7 +75,8 @@ class Standardize(tfk.layers.Layer):
 
     @property
     def var(self):
-        return self.m2 / self.count_float
+        m2 = tf.clip_by_value(self.m2, self.epsilon, np.inf)
+        return m2 / self.count_float
 
     def update(self, x):
         k = tf.reduce_sum(tf.ones_like(x)) / self.axis_size
@@ -90,8 +92,8 @@ class Standardize(tfk.layers.Layer):
 
     def standardize(self, data):
         if self.center:
-            return (data - self.mean) / self.var
-        return data / self.var
+            return (data - self.mean) / self.std
+        return data / self.std
 
     def call(self, data, training=True):
         if self.max_counts > self.max_counts:

abismal/merging/merging.py
@@ -115,10 +115,10 @@ class VariationalMergingModel(tfk.models.Model):
             sigiobs,
         ) = inputs
         if self.standardize_intensity is not None:
-            iobs = self.standardize_intensity(iobs)
-            sigiobs = self.standardize_intensity.standardize(sigiobs)
+            iobs = tf.ragged.map_flat_values(self.standardize_intensity, iobs)
+            sigiobs = tf.ragged.map_flat_values(self.standardize_intensity.standardize, sigiobs)
         if self.standardize_metadata is not None:
-            metadata = self.standardize_metadata(metadata)
+            metadata = tf.ragged.map_flat_values(self.standardize_metadata, metadata)
 
         training = kwargs.get('training', None)
         ll = None

abismal/scaling/scaling.py
@@ -226,8 +226,8 @@ class ImageScaler(tfk.layers.Layer):
                 kl_div = q_z - p_z
 
             kl_div = tf.reduce_mean(kl_div)
-            self.add_loss(kl_div)
-            self.add_metric(self.kl_weight * kl_div, name='KL_Σ')
+            self.add_loss(self.kl_weight * kl_div)
+            self.add_metric(kl_div, name='KL_Σ')
         z = tf.RaggedTensor.from_row_splits(tf.transpose(z), metadata.row_splits)
 
         return z