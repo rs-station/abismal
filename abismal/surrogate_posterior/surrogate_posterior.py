2e2d2874d164b86b5a4efba21d3247cf92d3972a
Kevin Dalton <kmdalton@pop-os.localdomain>
2024-05-15 17:50:27 -0400

    delete rogue q

--- abismal/surrogate_posterior/surrogate_posterior.py
+++ abismal/surrogate_posterior/surrogate_posterior.py
@@ -107,7 +107,7 @@ class PosteriorBase(tfk.models.Model):
             has_isigi = False
 
         try:
-            F,SIGF = self.get_flat_fsigf(q)
+            F,SIGF = self.get_flat_fsigf()
             has_fsigf = True
             data.update({
                 'F' : rs.DataSeries(F, dtype='F'),
