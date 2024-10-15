e1a95f0fa7d1dc533a4150fbd793fbc12befb477
Kevin Dalton <kmdalton@pop-os.localdomain>
2024-10-15 11:41:38 -0400

    rice base

---
+++ abismal/surrogate_posterior/rice.py
@@ -0,0 +1,31 @@
+import numpy as np
+import reciprocalspaceship as rs
+import tensorflow as tf
+from tensorflow_probability import distributions as tfd
+from tensorflow_probability import util as tfu
+from tensorflow_probability import bijectors as tfb
+import tf_keras as tfk
+from abismal.distributions import FoldedNormal as FoldedNormal
+from abismal.distributions.rice import Rice
+from abismal.surrogate_posterior import StructureFactorPosteriorBase
+from abismal.surrogate_posterior.folded_normal import FoldedNormalPosteriorBase
+
+
+
+#TODO: refactor with shared base class
+class RicePosteriorBase(FoldedNormalPosteriorBase):
+    """
+    A base class for creating Rician posteriors. 
+    """
+    def _distribution(self, loc, scale, low):
+        f = Rice(
+            loc, 
+            scale, 
+        )
+        q = tfd.TransformedDistribution(
+            f, 
+            tfb.Shift(low),
+        )
+        return q
+
+
