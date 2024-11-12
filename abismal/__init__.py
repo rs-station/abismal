22151f67f1df3c0d186185ab217301533894a7e9
Kevin Dalton <kmdalton@slac.stanford.edu>
2024-11-12 06:01:34 -0500

    future proof versioning

--- abismal/__init__.py
+++ abismal/__init__.py
@@ -1,13 +1,18 @@
-import reciprocalspaceship as rs
-# Version number for reciprocalspaceship
+## Version number for abismal
 def getVersionNumber():
-    import pkg_resources
+    version = None
+    try:
+        from setuptools.version import metadata
 
-    version = pkg_resources.require("abismal")[0].version
+        version = metadata.version("abismal")
+    except ImportError:
+        from setuptools.version import pkg_resources
+
+        version = pkg_resources.require("abismal")[0].version
+
     return version
 
-
-__version__ = getVersionNumber()
+__version__ = getVersionNumber() 
 
 from abismal.ragged import quiet
 from abismal import callbacks,distributions,io,layers,likelihood,merging,optimizers,scaling,surrogate_posterior,symmetry
