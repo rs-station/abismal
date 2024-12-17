bbc3ce4380cc817d73162888d3a868920295fff5
Kevin Dalton <kmdalton@slac.stanford.edu>
2024-12-17 09:52:14 -0500

    better default beta 2

--- abismal/command_line/parser/optimizer.py
+++ abismal/command_line/parser/optimizer.py
@@ -38,8 +38,8 @@ args_and_kwargs=(
         (
             "--beta-2",
         ),{
-            "help": "Second moment momentum parameter for Adam with default 0.999.",
-            "default": 0.999,
+            "help": "Second moment momentum parameter for Adam with default 0.9.",
+            "default": 0.9,
             "type": float,
         }
     ),
