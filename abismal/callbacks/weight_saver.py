0008b48326312f9c0964f7565e0f8e95e561282d
Kevin Dalton <kmdalton@slac.stanford.edu>
2024-12-17 17:11:32 -0500

    fix off-by-one error

--- abismal/callbacks/weight_saver.py
+++ abismal/callbacks/weight_saver.py
@@ -3,7 +3,7 @@ import tf_keras as tfk
 
 class WeightSaver(tfk.callbacks.ModelCheckpoint):
     def __init__(self, prefix, num_epochs, **kwargs):
-        fsize = int(math.log10(num_epochs))
+        fsize = int(math.log10(num_epochs)) + 1
         fstring = f'{prefix}/epoch_{{epoch:0{fsize}d}}.keras'
         super().__init__(filepath=fstring, **kwargs)
 
