63e58eb5464f9d58a17d129f825016c2c58ac288
Kevin Dalton <kmdalton@users.noreply.github.com>
2025-01-15 11:42:22 -0500

    Resolve #12

--- abismal/__init__.py
+++ abismal/__init__.py
@@ -14,5 +14,6 @@ def getVersionNumber():
 
 __version__ = getVersionNumber() 
 
+import pandas as pd #Workaround for scipy 1.15.1 (see https://github.com/rs-station/abismal/issues/12)
 from abismal.ragged import quiet
 from abismal import callbacks,distributions,io,layers,likelihood,merging,optimizers,scaling,surrogate_posterior,symmetry
