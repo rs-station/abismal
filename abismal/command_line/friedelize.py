8007421882fc1e044387bb04270d8742abb9f22d
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-01-17 07:23:21 -0800

    don't use file for filenames

--- abismal/command_line/friedelize.py
+++ abismal/command_line/friedelize.py
@@ -20,26 +20,26 @@ def is_friedel_plus(hkls, spacegroup):
     fplus = (isym % 2) == 1
     return fplus
 
-def friedelize_mtz(file, spacegroup=None):
-    mtz = rs.read_mtz(file)
-    base = file.removesuffix('.mtz')
+def friedelize_mtz(file_name, spacegroup=None):
+    mtz = rs.read_mtz(file_name)
+    base = file_name.removesuffix('.mtz')
     if spacegroup is not None:
         mtz.spacegroup = spacegroup
     fplus = is_friedel_plus(mtz.get_hkls(), mtz.spacegroup)
     mtz[fplus].write_mtz(base + '_plus.mtz')
     mtz[~fplus].write_mtz(base + '_minus.mtz')
 
-def friedelize_stream(file, spacegroup):
+def friedelize_stream(file_name, spacegroup):
     refl_start = "Reflections measured after indexing"
     refl_end = "End of reflections"
     if spacegroup is None:
         spacegroup = gemmi.SpaceGroup(1)
 
-    base = file.removesuffix('.stream')
+    base = file_name.removesuffix('.stream')
     plus = base + '_plus.stream'
     minus = base + '_minus.stream'
     in_refl_list = False
-    with iter(open(file)) as f, open(plus, 'w') as p, open(minus, 'w') as m:
+    with iter(open(file_name)) as f, open(plus, 'w') as p, open(minus, 'w') as m:
         for line in f:
             if line.startswith(refl_start):
                 in_refl_list = True
@@ -59,25 +59,25 @@ def friedelize_stream(file, spacegroup):
             else:
                 m.write(line)
 
-def friedelize_refl(file, spacegroup):
-    extension = file.split('.')[-1]
-    base = file.removesuffix('.' + extension)
+def friedelize_refl(file_name, spacegroup):
+    extension = file_name.split('.')[-1]
+    base = file_name.removesuffix('.' + extension)
     from dials.array_family import flex
-    refls = flex.reflection_table.from_file(file)
+    refls = flex.reflection_table.from_file(file_name)
     hkl = np.array(refls['miller_index'])
     plus = is_friedel_plus(hkl, spacegroup)
     refls.select(flex.bool(plus)).as_file(base + '_plus.' + extension)
     refls.select(flex.bool(~plus)).as_file(base + '_minus.' + extension)
 
-def friedelize(file, spacegroup='P 1'):
-    if file.endswith(".stream"):
-        friedelize_stream(file, spacegroup)
-    elif file.endswith(".pickle") or file.endswith(".refl"):
-        friedelize_refl(file, spacegroup)
-    elif file.endswith(".mtz"):
-        friedelize_mtz(file, spacegroup)
+def friedelize(file_name, spacegroup='P 1'):
+    if file_name.endswith(".stream"):
+        friedelize_stream(file_name, spacegroup)
+    elif file_name.endswith(".pickle") or file_name.endswith(".refl"):
+        friedelize_refl(file_name, spacegroup)
+    elif file_name.endswith(".mtz"):
+        friedelize_mtz(file_name, spacegroup)
     else:
-        raise ValueError(f"Cannot determine filetype for input file {file}")
+        raise ValueError(f"Cannot determine file for input file {file_name}")
 
 def main():
     parser = ArgumentParser(__doc__)
@@ -85,6 +85,6 @@ def main():
     parser.add_argument("--spacegroup", "-s", help="The space group of these data. If not supplied, attempt to infer it from the file falling back to P1", default=None)
 
     parser = parser.parse_args()
-    for file in parser.input_file:
-        friedelize(file, parser.spacegroup)
+    for file_name in parser.input_file:
+        friedelize(file_name, parser.spacegroup)
 
