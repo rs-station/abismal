18eaddfa6ace8cf321e43c43e7a3de7ce01fe10f
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-01-24 13:43:33 -0800

    fix friedel mtz column order

--- abismal/callbacks/mtz.py
+++ abismal/callbacks/mtz.py
@@ -23,12 +23,28 @@ class FriedelMtzSaver(MtzSaver):
     """
     Save friedelized inputs into a single mtz.
     """
+    column_names = (
+        'F(+)',
+        'SIGF(+)',
+        'F(-)',
+        'SIGF(-)',
+        # There's a bug with anomalous E-values
+        #'E(+)', 
+        #'SIGE(+)',
+        #'E(-)',
+        #'SIGE(-)',
+        'I(+)',
+        'SIGI(+)',
+        'I(-)',
+        'SIGI(-)',
+    )
     def save_mtz(self, epoch, seen=True):
         ds_plus,ds_minus = self.model.surrogate_posterior.to_datasets(seen=seen)
         data = rs.concat((
             ds_plus,
             ds_minus.apply_symop('-x,-y,-z'),
         )).unstack_anomalous()
+        data = data[[k for k in self.column_names if k in data]]
         data.write_mtz(f"{self.output_directory}/asu_0_epoch_{epoch+1}.mtz")
 
 
