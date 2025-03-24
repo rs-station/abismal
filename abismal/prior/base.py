36b8ad41b6edcaa96cbb2e19be235fc46db218ab
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-03-24 14:51:48 -0700

    inner peace

--- abismal/prior/base.py
+++ abismal/prior/base.py
@@ -1,13 +1,11 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow_probability import distributions as tfd
-from tensorflow_probability import util as tfu
-from tensorflow_probability import bijectors as tfb
 import tf_keras as tfk
-from abismal.symmetry import Op,ReciprocalASUCollection
 
 
 class PriorBase(tfk.layers.Layer):
     def distribution(self, asu_id, hkl):
         raise NotImplementedError(
             "Derived classes must implement distribution(asu_id, hkl) -> Distribution")
+
+    def flat_distribution(self):
+        raise NotImplementedError(
+            "Derived classes must implement flat_distribution() -> Distribution")
