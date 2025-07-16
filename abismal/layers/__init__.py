8f4b2e6ed2d6f816a823fb75e0f3144cfd28b65b
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-07-16 10:34:39 -0400

    rename skip to feed_forward

--- abismal/layers/__init__.py
+++ abismal/layers/__init__.py
@@ -1,4 +1,4 @@
-from .skip import FeedForward
+from .feed_forward import FeedForward
 from .combinations import ConvexCombination,ConvexCombinations,Average
 from .positional_encoding import ScaleRange,PositionalEncoding
 from .standardization import Standardize
