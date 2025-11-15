76fb26d218e42e19d0d91fb2b434d7f34a4176c2
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-11-15 04:38:42 -0500

    make groups a dict

--- abismal/command_line/parser/__init__.py
+++ abismal/command_line/parser/__init__.py
@@ -13,22 +13,22 @@ import abismal.command_line.parser.surrogate_posterior as surrogate_posterior
 import abismal.command_line.parser.tf as tf
 import abismal.command_line.parser.training as training
 
-groups = [
-    architecture,
-    io,
-    likelihood,
-    optimizer,
-    phenix,
-    priors,
-    ray,
-    surrogate_posterior,
-    tf,
-    training,
-]
+groups = {
+    'Architecture' : architecture,
+    'IO' : io,
+    'Likelihood' : likelihood,
+    'Optimizer' : optimizer,
+    'PHENIX' : phenix,
+    'Priors' : priors,
+    'Ray' : ray,
+    'Surrogate Posterior' : surrogate_posterior,
+    'TensorFlow' : tf,
+    'Training' : training,
+}
 
 from argparse import ArgumentParser
 parser = ArgumentParser(description=__doc__)
-for group in groups:
+for group in groups.values():
     g = parser.add_argument_group(group.title, group.description)
     for args,kwargs in group.args_and_kwargs:
         g.add_argument(*args, **kwargs)
