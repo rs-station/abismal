d83090d84d9beacf02fb20d34026d65329cc680a
Kevin Dalton <kmdalton@slac.stanford.edu>
2026-05-15 14:59:09 -0400

    much simpler

--- abismal/command_line/parser/io.py
+++ abismal/command_line/parser/io.py
@@ -2,14 +2,6 @@ title = "IO"
 description = "Arguments controlling file inputs and outputs."
 
 from multiprocessing import cpu_count
-class DetectCPUs():
-    def __call__(self):
-        nproc = cpu_count()
-        nproc = max(1, nproc - 1)
-        return nproc
-
-    def __str__(self):
-        return "Auto"
 
 args_and_kwargs=(
     (
@@ -28,7 +20,7 @@ args_and_kwargs=(
             "--num-cpus",
         ),{
         "help": "Number of CPUs to use for parsing CrystFEL .stream files with default 1.",
-        "default": DetectCPUs(),
+        "default": max(1, cpu_count() - 1),
         "type": int,
         }
     ),
